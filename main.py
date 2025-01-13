8976b8498603cc25a778e1cf1cac4db822b065f6
KenzaAITO <aito.kenza@gmail.com>
2025-01-13 15:58:30 +0100

    delete useless files

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import sys
 import os
 
-# Ajouter le chemin du dossier contenant DobotDllType.py
-sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
 
 import BlocVision.vision as vision
 import BlocRobot.InitPos as init
