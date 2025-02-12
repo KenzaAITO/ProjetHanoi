8f1596ba67963217306dc9af41fd1b4033bcafbb
KenzaAITO <aito.kenza@gmail.com>
2025-02-12 17:11:27 +0100

    Camera use NOK

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-/**
+""" 
 import sys
 import os
 
@@ -23,31 +23,33 @@ def main():
     
 if __name__ == "__main__":
     main()
-    
-
-**/
+   """  
 
 
 
+import BlocVision.vision as vision            # Importation de la classe Vision
+from BlocVision.vision import initialize_game
+import BlocRobot.InitPos as init              # Importation de la classe Robot
+from BlocAlgo.algo import hanoi_iteratif  # Importation de l'algorithme de la tour de Hanoï
 
-
-    # main.py
-
-from robot import Robot  # Importation de la classe Robot
-from vision import Vision  # Importation de la classe Vision
-from algo import hanoi_iterative  # Importation de l'algorithme de la tour de Hanoï
 
 def main():
     """
     Programme principal pour résoudre la Tour de Hanoï avec un robot et une caméra.
     """
+
+    print("Program Start:")
     
-    # === 1. INITIALISATION DES COMPOSANTS ===
+    # === 1. INITIALISATION DES COMPOSANTS === 
     print("Initialisation du robot...")
-    robot = Robot()  # Création de l'instance du robot
+    #robot = Robot()  # Création de l'instance du robot
+    intialisation = init.Robot()
 
     print("Initialisation de la caméra...")
-    camera = Vision()  # Création de l'instance de la caméra
+    #camera = Vision()  # La camera n'a pas encore de classe TO DO 
+    #intialisation.init() 
+
+    initialize_game()
 
     # === 2. ACQUISITION DE L'ÉTAT INITIAL ===
     print("Prise de photo pour analyser la tour d'origine...")
@@ -61,7 +63,7 @@ def main():
 
     # === 3. CALCUL DES DÉPLACEMENTS SELON L'ALGORITHME DE HANOÏ ===
     print("Calcul des déplacements...")
-    mouvements = hanoi_iterative(nb_palet_camera)  # Génération de la liste des déplacements
+    mouvements = hanoi_iteratif(nb_palet_camera)  # Génération de la liste des déplacements
 
     print(f"{len(mouvements)} déplacements générés.")
 
