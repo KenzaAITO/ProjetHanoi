e566838405b77fb67a4b89ef7711a8d8ad0720e7
KenzaAITO <aito.kenza@gmail.com>
2025-03-17 18:55:17 +0100

    separation generation de matrice et simulation. Simulation OK

--- BlocAlgo/algo.py
+++ BlocAlgo/algo.py
@@ -3,16 +3,35 @@ from PyQt6.QtGui import QPainter, QColor, QBrush
 from PyQt6.QtCore import Qt, QTimer
 import sys
 
-class Algorithm(QWidget):
+class HanoiAlgorithm:
     def __init__(self, n):
-        super().__init__()
         self.n = n
-        self.tower_positions = [100, 300, 500]
-        self.disk_widths = [60, 50, 40, 30][:n]  # Adjust based on n
         self.towers = {0: list(range(1, n + 1)), 1: [], 2: []}
         self.moves = []
         self.move_matrix = []
-        self.hanoi(n, 0, 1, 2)
+        self.generate_moves(n, 0, 1, 2)
+    
+    def generate_moves(self, n, source, auxiliary, destination):
+        if n == 1:
+            self.record_move(source, destination)
+            return
+        self.generate_moves(n - 1, source, destination, auxiliary)
+        self.record_move(source, destination)
+        self.generate_moves(n - 1, auxiliary, source, destination)
+    
+    def record_move(self, source, destination):
+        nb_palets_origine = len(self.towers[source])
+        nb_palets_destination = len(self.towers[destination])
+        self.moves.append((source, destination))
+        self.move_matrix.append((len(self.moves), source, destination, nb_palets_origine, nb_palets_destination))
+
+class HanoiSimulation(QWidget):
+    def __init__(self, algorithm):
+        super().__init__()
+        self.algorithm = algorithm
+        self.tower_positions = [100, 300, 500]
+        self.disk_widths = [60, 50, 40, 30][:self.algorithm.n]
+        self.towers = {0: list(range(1, self.algorithm.n + 1)), 1: [], 2: []}
         self.index = 0
         
         self.setWindowTitle("Tower of Hanoi - PyQt6")
@@ -21,21 +40,6 @@ class Algorithm(QWidget):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.next_move)
         self.timer.start(1000)
-        
-    def hanoi(self, n, source, auxiliary, destination):
-        if n == 1:
-            self.record_move(source, destination)
-            return
-        self.hanoi(n - 1, source, destination, auxiliary)
-        self.record_move(source, destination)
-        self.hanoi(n - 1, auxiliary, source, destination)
-    
-    def record_move(self, source, destination):
-        nb_palets_origine = len(self.towers[source])
-        nb_palets_destination = len(self.towers[destination])
-        self.moves.append((source, destination))
-        self.move_matrix.append((len(self.moves), source, destination, nb_palets_origine, nb_palets_destination))
-        self.move_disk(source, destination)
     
     def move_disk(self, source, destination):
         if self.towers[source]:
@@ -45,14 +49,14 @@ class Algorithm(QWidget):
             print(f"Erreur: Pas de disque à déplacer depuis la tour {source}")
         
     def next_move(self):
-        if self.index < len(self.moves):
-            source, destination = self.moves[self.index]
+        if self.index < len(self.algorithm.moves):
+            source, destination = self.algorithm.moves[self.index]
             self.move_disk(source, destination)
             self.index += 1
             self.update()
         else:
             self.timer.stop()
-            print("Matrice des mouvements:", self.move_matrix)
+            print("Matrice des mouvements:", self.algorithm.move_matrix)
         
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -76,6 +80,7 @@ class Algorithm(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Algorithm(4)
+    algorithm = HanoiAlgorithm(4)
+    window = HanoiSimulation(algorithm)
     window.show()
     sys.exit(app.exec())
