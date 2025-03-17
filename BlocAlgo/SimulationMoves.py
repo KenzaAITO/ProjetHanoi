95fe275ecca0ddaad6c4e272f88e1e2903d2c33d
KenzaAITO <aito.kenza@gmail.com>
2025-03-17 20:58:27 +0100

    simulation okay jusqu'a 5 palets

--- BlocAlgo/SimulationMoves.py
+++ BlocAlgo/SimulationMoves.py
@@ -1,16 +1,17 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QPainter, QColor, QBrush
 from PyQt6.QtCore import Qt, QTimer
 
-from BlocAlgo.HanoiIterative import HanoiIterative  # Assuming the correct import
+from BlocAlgo.HanoiIterative import HanoiIterative  # Ensure this import is correct
+
 class SimulationMoves(QWidget):
 
     def __init__(self, algorithm):
         super().__init__()
         self.algorithm = algorithm
         self.tower_positions = [100, 300, 500]
-        self.palet_widths = [60, 50, 40, 30][:self.algorithm.nb_palet_camera]
+        self.palet_widths = [80, 70, 60, 50, 40][:self.algorithm.nb_palet_camera]  # Support for 5 disks
         self.towers = {0: list(range(1, self.algorithm.nb_palet_camera + 1)), 1: [], 2: []}
         self.index = 0
         self.movements = self.algorithm.get_move_matrix()  # Ensure the movements are retrieved properly
@@ -47,24 +48,27 @@ class SimulationMoves(QWidget):
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
         painter.fillRect(self.rect(), QColor(255, 255, 255))
 
+        # Draw towers
         for x in self.tower_positions:
             painter.setBrush(QBrush(QColor(0, 0, 0)))
             painter.drawRect(x - 10, 100, 20, 200)
             painter.drawEllipse(x - 15, 80, 30, 30)
 
+        # Draw disks on towers
         for i, tower in self.towers.items():
             for j, palet in enumerate(tower):
                 palet_index = palet - 1
-                painter.setBrush(QBrush(QColor(0, 0, 255)))
-                painter.drawRect(
-                    self.tower_positions[i] - self.palet_widths[palet_index] // 2, 
-                    280 - j * 20, 
-                    self.palet_widths[palet_index], 20
-                )
+                if palet_index < len(self.palet_widths):  # Ensure index doesn't exceed available sizes
+                    painter.setBrush(QBrush(QColor(0, 0, 255)))
+                    painter.drawRect(
+                        self.tower_positions[i] - self.palet_widths[palet_index] // 2, 
+                        280 - j * 20, 
+                        self.palet_widths[palet_index], 20
+                    )
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    algorithm = HanoiIterative(4)
+    algorithm = HanoiIterative(5)  # Set to 5 disks
     window = SimulationMoves(algorithm)
     window.show()
     sys.exit(app.exec())
