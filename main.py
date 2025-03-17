1d7ce8b85d99b7b07a1a2de1055b1e393d79a346
KenzaAITO <aito.kenza@gmail.com>
2025-03-17 01:40:22 +0100

    update main with class camera NOK

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-""" 
-import sys
-import os
+#import BlocVision.vision as vision            # Importation de la classe Vision
+#import BlocRobot.DobotControl as robot
+#import BlocRobot.InitPos as init              # Importation de la classe Robot
 
-
-import BlocVision.vision as vision
-from BlocVision.vision import initialize_game
-import BlocRobot.InitPos as init
-
-def main():
-    print("Program Start:")
-    #partie initialisation 
-    intialisation = init.Robot()
-    #intialisation.init() 
-
-    initialize_game()
-
-    #lancement de l'interface
-    
-
-    
-if __name__ == "__main__":
-    main()
-   """  
-
-
-
-import BlocVision.vision as vision            # Importation de la classe Vision
-import BlocRobot.InitPos as init              # Importation de la classe Robot
 from BlocAlgo.algo import hanoi_iteratif  # Importation de l'algorithme de la tour de Hanoï
-import BlocRobot.DobotControl as robot
+from BlocVision.vision import CameraProcessor, DetectionInterface
+from BlocRobot.DobotControl import DobotControl
+from BlocRobot.InitPos import Robot
 
 def main():
     """
@@ -39,32 +16,39 @@ def main():
     
     # === 1. INITIALISATION DES COMPOSANTS === 
     print("Initialisation du robot...")
-    #robot = Robot()  # Création de l'instance du robot
-    init.Robot()
+    robot = Robot()  # Création de l'instance du robot
+    #init.Robot()
     #boucle pour afficher que tous est correct 
 
     print("Initialisation de la caméra...")
-    #camera = Vision()  # La camera n'a pas encore de classe TO DO 
     #intialisation.init() 
-    vision.initialize_game()
+    #vision.initialize_game()
+    processor = CameraProcessor()
 
     # === 2. ACQUISITION DE L'ÉTAT INITIAL ===
     print("Prise de photo pour analyser la tour d'origine...")
-    #vision = vision.capture_initial_image()
+    frame = processor.capture_image()
 
-    nb_palet_camera = vision.display_nb_palets()
+    if frame is not None:
+        num_discs, _ = processor.detect_discs(frame)
+        print(f"Nombre de palets détectés : {num_discs}")
+        if num_discs == 0:
+            print("Erreur : Aucun palet détecté. Vérifiez la caméra.")
+            return
+    
+    #On valide mtn le nombre de palets par l'utilisateur 
+    interface = DetectionInterface(num_discs)
+    validated_count = interface.show_interface()
+    print(f"Nombre de palets détectés : {validated_count}")
 
-    if nb_palet_camera == 0:
-        print("Erreur : Aucun palet détecté. Vérifiez la caméra.")
-        return
-
-    print(f"Nombre de palets détectés : {nb_palet_camera}")
+    #nb_palet_camera = vision.display_nb_palets()
 
     # === 3. CALCUL DES DÉPLACEMENTS SELON L'ALGORITHME DE HANOÏ ===
     print("Calcul des déplacements...")
-    mouvements = hanoi_iteratif(nb_palet_camera)  # Génération de la liste des déplacements
+    mouvements = hanoi_iteratif(validated_count)  # Génération de la liste des déplacements
 
-    #print(f"{len(mouvements)} déplacements générés.")
+
+    print(f"{len(mouvements)} déplacements générés.")
 
     # === 4. EXÉCUTION DES DÉPLACEMENTS PAR LE ROBOT ===
     #for move in mouvements:
@@ -83,7 +67,6 @@ if __name__ == "__main__":
     main()
     
 #TODO: Fonction Déplacer
-#TODO: Fonction 
 #TODO: Généré les données en transit
 #TODO: Généré la liste des mouvements (position)
 
